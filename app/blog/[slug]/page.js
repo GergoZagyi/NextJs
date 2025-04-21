106f26aef167192425a6e98cbc2a281bd8ffa6ba
KaaronZ <geri0525@gmail.com>
2025-04-21 18:08:25 +0200

    This is some devious stuff

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { marked } from 'marked';
+
+export async function generateStaticParams() {
+  const postsDir = path.join(process.cwd(), 'content', 'posts');
+  const files = fs.readdirSync(postsDir);
+
+  return files.map(filename => ({
+    slug: filename.replace('.md', '')
+  }));
+}
+
+export default async function BlogPost({ params }) {
+  const { slug } = params;
+  const filePath = path.join(process.cwd(), 'content', 'posts', `${slug}.md`);
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const { data, content } = matter(fileContent);
+  const htmlContent = marked(content);
+
+  return (
+    <article className="prose mx-auto px-4 py-8">
+      <h1>{data.title}</h1>
+      <p className="text-sm text-gray-500">
+        By {data.author} on {data.date}
+      </p>
+      {data.image && <img src={data.image} alt={data.title} className="my-4" />}
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    </article>
+  );
+}
---
+++ content/posts/first-post.md
@@ -0,0 +1,13 @@
+---
+title: "My First Blog Post"
+date: "2025-04-20"
+author: "Zagyi Gergő"
+image: "/images/sample.jpg"  
+---
+
+Welcome to my first blog post!
+
+This is an example of how content is written using Markdown. You can write **bold**, _italic_, or even include [links](https://example.com).
+
+- This is a list item
+- Another item
